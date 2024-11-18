d3c5966b6bc0a7903d351d062543e97c590f964e
Mickaël Douceré <mickamickadou@gmail.com>
2024-11-18 20:39:21 +0100

    Mise en forme du texte et ajout stack

--- src/Components/ProjectDetails/ProjectDetails.jsx
+++ src/Components/ProjectDetails/ProjectDetails.jsx
@@ -1,23 +1,40 @@
 import './projectDetails.scss'
 
-function ProjectDetails({ description, learned, stack }) {
+function ProjectDetails({ description, learned, stacks }) {
+  console.log('stacks', stacks)
+
   return (
     <div className="projectDetails">
       <div className="projectDetails_container">
         <div className="projectDetails_content">
           <h2 className="projectDetails_title">Description du projet</h2>
-          <p className="projectDetails_text">{description}</p>
+          <p
+            className="projectDetails_text"
+            dangerouslySetInnerHTML={{ __html: description }}
+          ></p>
         </div>
         <div className="projectDetails_content">
           <h2 className="projectDetails_title">Ce que j'ai appris</h2>
-          <p className="projectDetails_text">{learned}</p>
+          <p
+            className="projectDetails_text"
+            dangerouslySetInnerHTML={{ __html: learned }}
+          ></p>
         </div>
       </div>
       <div className="projectDetails_content projectDetails_content_last">
         <h2 className="projectDetails_title">
-          Tecnnologies et outils utilisés
+          Technologies et outils utilisés
         </h2>
-        <div>{stack}</div>
+        <div className="technologies-container">
+          {stacks.map((stack) => (
+            <img
+              className="technology"
+              key={stack}
+              src={stack}
+              alt="logo de la technologie"
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
