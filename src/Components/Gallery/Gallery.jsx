9b6be0e75f0bc4cc024e254e8ee39d94eebfd7ba
Mickaël Douceré <mickamickadou@gmail.com>
2024-11-18 20:35:59 +0100

    Accessibilité au clavier du carousel

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -7,6 +7,7 @@ import ArrowLeft from '../../Assets/Arrow/ArrowLeft.svg'
 
 function Gallery({ pictures }) {
   const [currentPicture, setCurrentPicture] = useState(0)
+  console.log('pictures', pictures)
 
   const nextSlide = () => {
     setCurrentPicture((currentPicture + 1) % pictures.length)
@@ -21,45 +22,57 @@ function Gallery({ pictures }) {
     setCurrentPicture(currentPicture - 1)
   }
 
+  const handleKeyPress = (event, action) => {
+    if (event.key === 'Enter') {
+      action()
+    }
+  }
+
   return (
     <>
-      <div className="works-carousel ">
-        <div className="works-carousel_image-container">
+      <div className="carousel-container">
+        <div className="works-carousel ">
           {pictures.map((picture, index) =>
             currentPicture === index ? (
               <img
                 key={picture}
                 src={picture}
                 alt=""
-                className={'works-carousel_slide'}
+                className="works-carousel_slide"
               />
             ) : null
           )}
+
+          {pictures.length === 1 ? null : (
+            <span className="slideCounter">
+              {currentPicture + 1}/{pictures.length}
+            </span>
+          )}
+
+          {pictures.length > 1 && (
+            <img
+              className="btn arrowLeft"
+              tabindex="0"
+              aria-label="Précédent"
+              src={ArrowLeft}
+              alt="icon flèche gauche"
+              onClick={previousSlide}
+              onKeyPress={(event) => handleKeyPress(event, previousSlide)}
+            />
+          )}
+
+          {pictures.length > 1 && (
+            <img
+              className="btn arrowRight"
+              tabindex="0"
+              aria-label="suivant"
+              src={ArrowRight}
+              alt="icon flèche droite"
+              onClick={nextSlide}
+              onKeyPress={(event) => handleKeyPress(event, nextSlide)}
+            />
+          )}
         </div>
-
-        {pictures.length === 1 ? null : (
-          <span className="slideCounter">
-            {currentPicture + 1}/{pictures.length}
-          </span>
-        )}
-
-        {pictures.length > 1 && (
-          <img
-            className="btn arrowLeft"
-            src={ArrowLeft}
-            alt="icon flèche gauche"
-            onClick={previousSlide}
-          />
-        )}
-
-        {pictures.length > 1 && (
-          <img
-            className="btn arrowRight"
-            src={ArrowRight}
-            alt="icon flèche droite"
-            onClick={nextSlide}
-          />
-        )}
       </div>
     </>
   )
