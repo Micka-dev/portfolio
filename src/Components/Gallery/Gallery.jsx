41153c8c05bb1e4abe540fd47eb4a38c1dcf6af3
Mickaël Douceré <mickamickadou@gmail.com>
2024-11-20 19:46:10 +0100

    Nouvelles modifications pour qu'il n'y ait plus d'interférences dans le défilement des slides au clic sur les flèches => maintenant le carousel fonctionne au clavier, au clic, et par glissement

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-
 import './gallery.scss'
-
 import ArrowRight from '../../Assets/Arrow/ArrowRight.svg'
 import ArrowLeft from '../../Assets/Arrow/ArrowLeft.svg'
 
@@ -15,87 +13,76 @@ function Gallery({ pictures }) {
   }
 
   const previousSlide = () => {
-    const newCurrentPicture = currentPicture - 1
-    if (newCurrentPicture < 0) {
-      setCurrentPicture(pictures.length - 1)
-      return
-    }
-    setCurrentPicture(currentPicture - 1)
+    setCurrentPicture((currentPicture - 1 + pictures.length) % pictures.length)
   }
 
   const handleTouchStart = (e) => {
     setTouchStartX(e.targetTouches[0].clientX)
   }
+
   const handleTouchMove = (e) => {
     setTouchEndX(e.targetTouches[0].clientX)
   }
+
   const handleTouchEnd = () => {
-    if (touchStartX - touchEndX > 50) {
-      nextSlide()
-    }
-    if (touchStartX - touchEndX < -50) {
-      previousSlide()
-    }
+    const distance = touchStartX - touchEndX
+    if (distance > 50) nextSlide()
+    if (distance < -50) previousSlide()
   }
 
   const handleKeyPress = (event, action) => {
-    if (event.key === 'Enter') {
-      action()
-    }
+    if (event.key === 'Enter') action()
   }
 
   return (
-    <>
-      <div className="carousel-container">
-        <div
-          className="works-carousel"
-          onTouchStart={handleTouchStart}
-          onTouchMove={handleTouchMove}
-          onTouchEnd={handleTouchEnd}
-        >
-          {pictures.map((picture, index) =>
-            currentPicture === index ? (
+    <div className="carousel-container">
+      <div className="works-carousel">
+        {pictures.map(
+          (picture, index) =>
+            currentPicture === index && (
               <img
                 key={picture}
                 src={picture}
                 alt=""
                 className="works-carousel_slide"
+                onTouchStart={handleTouchStart}
+                onTouchMove={handleTouchMove}
+                onTouchEnd={handleTouchEnd}
               />
-            ) : null
-          )}
+            )
+        )}
 
-          {pictures.length === 1 ? null : (
-            <span className="slideCounter">
-              {currentPicture + 1}/{pictures.length}
-            </span>
-          )}
+        {pictures.length > 1 && (
+          <span className="slideCounter">
+            {currentPicture + 1}/{pictures.length}
+          </span>
+        )}
 
-          {pictures.length > 1 && (
-            <img
-              className="btn arrowLeft"
-              tabindex="0"
-              aria-label="Précédent"
-              src={ArrowLeft}
-              alt="icon flèche gauche"
-              onClick={previousSlide}
-              onKeyPress={(event) => handleKeyPress(event, previousSlide)}
-            />
-          )}
+        {pictures.length > 1 && (
+          <img
+            className="btn arrowLeft"
+            tabIndex="0"
+            aria-label="Précédent"
+            src={ArrowLeft}
+            alt="icon flèche gauche"
+            onClick={previousSlide}
+            onKeyPress={(event) => handleKeyPress(event, previousSlide)}
+          />
+        )}
 
-          {pictures.length > 1 && (
-            <img
-              className="btn arrowRight"
-              tabindex="0"
-              aria-label="suivant"
-              src={ArrowRight}
-              alt="icon flèche droite"
-              onClick={nextSlide}
-              onKeyPress={(event) => handleKeyPress(event, nextSlide)}
-            />
-          )}
-        </div>
+        {pictures.length > 1 && (
+          <img
+            className="btn arrowRight"
+            tabIndex="0"
+            aria-label="Suivant"
+            src={ArrowRight}
+            alt="icon flèche droite"
+            onClick={nextSlide}
+            onKeyPress={(event) => handleKeyPress(event, nextSlide)}
+          />
+        )}
       </div>
-    </>
+    </div>
   )
 }
 
